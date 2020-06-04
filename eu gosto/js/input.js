b67ea0fe938437da122903815116ecfaa981797b
Daniel-Zacarias <daniel.santos@bandtec.com.br>
2020-06-04 17:04:47 -0300

    adicionando mais coisas na tela de cadastro

--- eu gosto/js/input.js
+++ eu gosto/js/input.js
@@ -1,11 +1,14 @@
-const input = document.querySelectorAll("input");  //pega todos os elementos com a class .mudar(const valor contante)
+// const campeao = ['Aatrox', 'Ahri','Akali', 'Alistar' , 'Amumu','Anivia','Annie','Ashe', , 'Aphelios','Aurelion Sol','Azir',
+// 'Blitz','Brand','Braum','Caitlyn', 'Camille', 'Cassiopeia', "Cho'Gath", 'Corki', 'Darius', 'Diana', 'Dr. Mundo','Draven','Draven', 'Ekko', 'Elise','Evelynn', 'Ezreal','Fiddlesticks' ,'Fiora', 'Fizz', 'Galio', 'GangPlank','Garen', 'Gnar','Gragas', 'Graves', 'Hecarim', 'Heimerdinger', 'Illaoi',
+// 'Irelia', 'Ivern', 'Janna', 'Jarvan', 'Jax','jayce', 'Jhin', 'Jinx', "Kai'sa", 'Kalista', 'Karma', 'Karthus', 'Kassadin','Katarina', 'Kayle', 'Kayn', 'Kennen', "Kha'zix", 'Kindreds', 'Kled', "Kog'waw",'Kled', 'Lucian', 'Lulu', 'Lux', 'Malphite', 'Malzahar','Maokai', 'Master Yi', 'Miss Fortune', 'Mordekaiser', 'Morgana', 'Nami', 'Nasus', 'Nautilus','Neeko', 'NidaLee', 'Nocturne', 'Nunu e Willump', 'Olaf', 'Orianna', 'Ornn', 'Pantheon', 'Poppy', 'Pyke','Qiyana', 'Quinn','rakan', 'Rammus', "Rek'Sai", 'Renekton', 'Rengar','Riven','Rumble','Ryze','Sejuani','Senna','Sett','Shaco','Shen','Shyvana','Singed','Sion','Sivir','Skarner','Sona','Soraka','Swain','Sylas',
+// 'Syndra','Tahm Kench','Taliyah','Talon','Taric','Teemo','Thresh','Tristana','Trundle','Tryndamere','Twisted Fate','Twitch','Udyr','Urgot','Varus','Vayne','Veigar',"Vel'Koz",'Vi','Viktor','Vladimir','Volibear','Warwick','Wukong',
+// 'Xayah','Xerath','Xin Zhao','Yasuo','Yorick','Yuumi','Zac','Zed','Ziggs','Zillean', 'Zoe', 'Zyra'
+// ]
 
-for (let i = 0; i < input.length; i++) {    //contador para todos os elementos
-    input[i].addEventListener("focus", function () { // quando o campo texto for usado
-        this.offsetParent.classList.add("ativar"); // adicione a div pai a class .ativar
-    }) 
-    input[i].addEventListener("blur", function () { //quando o campo de texto deixar de ser usado
-        if (this.value == "") // caso valor do texto for ""
-            this.offsetParent.classList.remove("ativar");  //retira a class .ativar
-    })
-}
+// for(let cont = 0;cont< campeao.length; cont++){
+//     slcCampeao.innerHTML += `<option value="${cont+1}">${campeao[cont]}</option>` 
+// }
+
+// console.log(campeao.length);
+
+
